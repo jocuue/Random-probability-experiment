0642c206bac6ec50f2165a67e60a063cb28c51af
jocuue <jocuue.coding@gmail.com>
2026-08-09 21:11:53 -0400

    Update probability.py

--- probability.py
+++ probability.py
@@ -1,7 +1,5 @@
 import experiment
 
-
-
 ## Uses experiment results and shows the probability of the chosen number and the amount of attempts taken.
 probability = experiment.turns_with_number / experiment.attempts
 
